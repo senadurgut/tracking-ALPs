58a1df8db1b1dc0a552201735d03c60ee0fa473f
Sena Durgut <abrekk01@gmail.com>
2026-03-19 15:22:08 -0400

    adding the ALP UFO model from Gonzalo

---
+++ model/ALP-h_UFO/CT_couplings.py
@@ -0,0 +1,11 @@
+# This file was automatically created by FeynRules 2.3.28
+# Mathematica version: 11.0.0 for Linux x86 (64-bit) (July 28, 2016)
+# Date: Fri 24 Jan 2020 12:20:09
+
+
+from object_library import all_couplings, Coupling
+
+from function_library import complexconjugate, re, im, csc, sec, acsc, asec, cot
+
+
+
---
+++ model/ALP-h_UFO/coupling_orders.py
@@ -0,0 +1,16 @@
+# This file was automatically created by FeynRules 2.3.28
+# Mathematica version: 11.0.0 for Linux x86 (64-bit) (July 28, 2016)
+# Date: Fri 24 Jan 2020 12:20:09
+
+
+from object_library import all_orders, CouplingOrder
+
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
